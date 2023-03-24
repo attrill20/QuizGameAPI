d2aff8e6622087385dda678d9700dad3ac590238
Tracey Williams <traceywilliams@msn.com>
2023-03-24 11:40:50 +0000

    API and Buttons working,

--- main.js
+++ main.js
@@ -5,7 +5,23 @@
 // await the json-formatted data from the response
 // assign question variable to the question property of the data object
 // assign answer variable to the correct_answer property of the data object
-// console log the question and answer
+// replace id question with api question
+// replace answer with api answer
+// create a new "Generate New Question" button which runs the getQuestions function again
+
+// add event listener to "Generate New Question" button
+
+
+// add eventlistener to true and false buttons 
+//  by using an if else statement to check if the answer is true or false.  
+// compare box id clicked to answer from API at opentdb.com
+// if the two match is true then display alert saying "correct"
+// if the two match is false then display alert saying "incorrect"
+
+
+
+
+
 
 async function getQuestions() {
     const response = await fetch('https://opentdb.com/api.php?amount=1&difficulty=easy&type=boolean');
@@ -13,7 +29,13 @@ async function getQuestions() {
     console.log(data);
     const question = data.results[0].question;
     console.log(question);
+    document.querySelector('#question').innerHTML = question;
+    console.log("#question");
     const answer = data.results [0].correct_answer;
+    document.querySelector('#answer').innerHTML = answer;
     console.log(answer);
 }
 getQuestions();
+
+const generateButton = document.querySelector('#newQ'); 
+generateButton.addEventListener('click', getQuestions);
