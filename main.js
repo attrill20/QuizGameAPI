32501bb7ccb44faa5bc122c987bf368174b19f85
James Attrill <attrill20@gmail.com>
2023-03-24 12:31:12 +0000

    true and false buttons working

--- main.js
+++ main.js
@@ -8,17 +8,16 @@
 // replace id question with api question
 // replace answer with api answer
 // create a new "Generate New Question" button which runs the getQuestions function again
-
 // add event listener to "Generate New Question" button
-
-
 // add eventlistener to true and false buttons 
 //  by using an if else statement to check if the answer is true or false.  
 // compare box id clicked to answer from API at opentdb.com
 // if the two match is true then display alert saying "correct"
 // if the two match is false then display alert saying "incorrect"
 
-
+// scoreboard
+// ends after you get 3 questions incorrect (3 lives)
+// styling design
 
 
 
@@ -39,3 +38,27 @@ getQuestions();
 
 const generateButton = document.querySelector('#newQ'); 
 generateButton.addEventListener('click', getQuestions);
+
+const trueButton = document.querySelector('#true');
+trueButton.addEventListener('click', alertFunction);
+
+const falseButton = document.querySelector('#false');
+falseButton.addEventListener('click', alertFunction);
+
+
+
+// we want this function below to run when the true or false button is clicked
+// we want to compare the 'value' of the button clicked to the answer from the API
+// if the two match then display alert saying "correct"
+// if the two don't match then display alert saying "incorrect"
+
+function alertFunction() {
+    if (this.value === document.querySelector('#answer').innerHTML) {
+        alert('Correct!');
+    } else {
+        alert('Incorrect!');
+    }
+}
+    // hide the answer on the page
+    document.querySelector('#answer').style.display = 'none';
+
